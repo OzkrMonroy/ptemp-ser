5cb3842c98b23755b2397e9f47b9f863067d43c4
Oscar Monroy <oscarmonroy657@gmail.com>
2021-04-19 18:05:32 -0600

    Projects routes has been created.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const connectToDB = require('./config/db');
 
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
+const projectsRouter = require('./routes/projects');
 
 const app = express();
 connectToDB();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json({ extended: true }));
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/projects', projectsRouter);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor running on Port ${PORT}`);
---
+++ routes/projects.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const projectController = require('../controllers/projectController');
+const { check } = require('express-validator');
+
+const router = express.Router();
+
+router.post('/', [
+  check('name', 'El nombre del proyecto es obligatorio.').not().isEmpty(),
+  check('description', 'La descripción es obligatoria').not().isEmpty(),
+  check('imageUrl', 'La url de imagen es obligatoria').not().isEmpty(),
+  check('demoUrl', 'La url del demo es obligatoria').not().isEmpty(),
+  check('codeUrl', 'La url del repositorio es obligatoria').not().isEmpty().isURL(),
+  check('tags', 'Los tags son obligatorios').isArray({min: 1})
+], projectController.createProject);
+
+router.put('/', [
+  check('name', 'El nombre del proyecto es obligatorio.').not().isEmpty(),
+  check('description', 'La descripción es obligatoria').not().isEmpty(),
+  check('imageUrl', 'La url de imagen es obligatoria').not().isEmpty(),
+  check('demoUrl', 'La url del demo es obligatoria').not().isEmpty(),
+  check('codeUrl', 'La url del repositorio es obligatoria').not().isEmpty().isURL(),
+  check('tags', 'Los tags son obligatorios').isArray({min: 1})
+], projectController.updateProject);
+
+router.delete('/:id', projectController.deleteProject);
+
+router.get('/', projectController.fetchAllProjects);
+
+module.exports = router;
